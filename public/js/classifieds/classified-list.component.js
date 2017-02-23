f8c1413af8ca73c261333665f271913808a21311
Kyle Gray <th33nd3r@gmail.com>
2017-02-23 10:59:53 -0700

    styled page

--- public/js/classifieds/classified-list.component.js
+++ public/js/classifieds/classified-list.component.js
@@ -14,9 +14,16 @@ function controller($http){
   vm.newPost = newPost;
   vm.updatePost = updatePost;
   vm.deletePost = deletePost;
+  vm.onEditButton = onEditButton;
+  vm.getClassifieds = getClassifieds;
 
 
   function onInit(){
+    vm.classifieds = [];
+    vm.getClassifieds();
+  }
+
+  function getClassifieds(){
     $http.get('/classifieds')
     .then((results)=>{
       vm.classifieds = results.data;
@@ -29,6 +36,7 @@ function controller($http){
   function newPost(input){
     console.log(input);
     $http.post('classifieds', input).then((results) => {
+      vm.getClassifieds();
     })
     .catch((err) =>{
       console.log(err);
@@ -37,15 +45,28 @@ function controller($http){
 
   function updatePost(input){
     $http.patch(`classifieds/${input.id}`, input).then((results) => {
+      vm.getClassifieds();
     })
     .catch((err) =>{
       console.log(err);
     });
   }
 
-  function deletePost(input){
-    $http.delete(`classifieds/${input.id}`)
+  function onEditButton(classified){
+    classified.showEditForm = !classified.showEditForm;
+    vm.updateclassifieds = {
+      id: classified.id,
+      title: classified.title,
+      price: classified.price,
+      image: classified.item_image,
+      description: classified.description,
+    };
+  }
+
+  function deletePost(classifiedID){
+    $http.delete(`classifieds/${classifiedID}`)
       .then((results) => {
+        vm.getClassifieds();
       })
       .catch((err) => {
         console.log(err);
